53e1aff5a06733862651b56865f255343be465c1
spacecodee <spacecodee@gmail.com>
2022-11-10 15:35:34 -0500

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from "./view/modules/app/app.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-  },
   {
     path: 'formulas',
     loadChildren: () => import('./view/modules/formulas/formulas.module').then(m => m.FormulasModule)
